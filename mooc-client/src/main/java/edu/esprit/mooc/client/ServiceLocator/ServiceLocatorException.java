2e48fd71fecb492915a10d05c3bd058f090d7d0e
mitch4u2 <mohamed.hajjej@esprit.tn>
2016-04-14 21:52:33 +0100

    first Web Commit

--- mooc-client/src/main/java/edu/esprit/mooc/client/ServiceLocator/ServiceLocatorException.java
+++
@@ -1,8 +0,0 @@
-package edu.esprit.mooc.client.ServiceLocator;
-
-public class ServiceLocatorException extends RuntimeException {
-  
-	public ServiceLocatorException(Throwable cause) {
-		super(cause);
-	}
-}
